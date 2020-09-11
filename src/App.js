117fb1e3b1316ba917ab042e2b43f2ac64a5a8ce
pandora100 <rramossh1@gmail.com>
2020-09-11 04:20:32 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React,{useEffect, useRef, useState} from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function useInstance(instance = {}) {
+    // assertion: instance && typeof instance === "object"
+    const ref = useRef(instance);
+    return ref.current;
+}
+export default function App({children}) {
+  const [activeSlide, setActiveSlide] = useState(0);
+    const activeSlideRef = useRef(null);
+    const inst = useInstance({first: true});
+
+    console.log("render");
+
+    useEffect(() => {
+        // *** After render, don't do anything, just remember we've seen the render
+        if (inst.first) {
+            console.log("set false");
+            inst.first = false;
+        } else if (activeSlideRef.current) {
+            console.log("scroll");
+            activeSlideRef.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'end',
+              inline: 'end'
+            });
+        }
+    }, [activeSlide]);
+
+    const moveLeft = Math.max(0, activeSlide - 1);
+    const moveRight = Math.min(children.length - 1, activeSlide + 1);
+
+    return (
+        <React.Fragment>
+          <button onClick={() => setActiveSlide(moveLeft)}>PREV</button>
+          <div id="test">
+            {children.map((child, i) => {
+              const active = i === activeSlide;
+              return (
+                <div className={`slide ${active ? "active" : ""}`} ref={active ? activeSlideRef : null} id={`slide-${i}`} key={`slide-${i}`}>
+                  {child}
+                </div>
+              );
+            })}
+          </div>
+
+          <button onClick={() => setActiveSlide(moveRight)}>NEXT</button>
+        </React.Fragment>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<div style={{display :'flex', flexDirection:'rows',alignItems:'center',justifyContent:'center'}}>  
+<App>
+      <div>slide 0 </div>
+      <div>slide 1 </div>
+      <div>slide 2 </div>
+      <div>slide 3 </div>
+      <div>slide 4 </div>
+      <div>slide 5 </div>
+      <div>slide 6 </div>
+      <div>slide 7 </div>
+      <div>slide 8 </div>
+      <div>slide 9 </div>
+    </App>
+</div>
+
+
+, document.getElementById("root"));
